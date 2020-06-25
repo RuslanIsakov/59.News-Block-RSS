885e2f7acb30dfd50444b4225ffb77ad5b3365ed
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-06-25 10:46:58 +0600

    Update index.php

--- old/index.php
+++ old/index.php
@@ -6,3 +6,6 @@ header('Content-Type: text/html; charset=utf-8');
  * Parse XML data into an array structure                 *
  * Usage: array parse_rss ( string data )                 *
  **********************************************************/
+function parse_rss($reg_exp, $xml_data) {
+    preg_match_all($reg_exp, $xml_data, $temp);
+    return array(
