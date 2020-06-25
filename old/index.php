eb87423b38fc420b3c55f7875deea6781e9ba48f
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-06-25 10:46:51 +0600

    Update index.php

--- old/index.php
+++ old/index.php
@@ -1,1 +1,8 @@
-
+<link rel="stylesheet" href="style.css">
+<title> News </title>
+<?php
+header('Content-Type: text/html; charset=utf-8');
+/**********************************************************
+ * Parse XML data into an array structure                 *
+ * Usage: array parse_rss ( string data )                 *
+ **********************************************************/
