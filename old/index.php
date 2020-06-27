a510b25e11a470018825f5309a237da2080c67da
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-06-27 14:56:50 +0600

    Update index.php

--- old/index.php
+++ old/index.php
@@ -45,3 +45,6 @@ if ( $xml_data = file_get_contents($url) ) {
     $rss_data = parse_rss($reg_exp, $xml_data);
  echo output_rss($pattern, $rss_data, 80);
 }
+/**********************************************************
+ * The END                                                *
+ **********************************************************/
